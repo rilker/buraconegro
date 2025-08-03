89439a6dd474ad8678a374b2c047edf7e6ae96de
rilkercls <rilker_surf@hotmail.com>
2025-08-03 18:13:37 -0300

    Deploy inicial do site Buraco Negro

---
+++ sources/Experience/assets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            // { name: 'cupolaModel', source: '/assets/models/cupola.glb' },
+        ]
+    }
+]
